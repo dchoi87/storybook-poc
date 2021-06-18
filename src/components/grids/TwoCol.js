2361a42c162e3eb90c4492538536f7c8eb49cc80
Daniel Choi <dchoi87@gmail.com>
2021-06-18 13:08:54 -0700

    Cleanup

--- src/components/grids/TwoCol.js
+++ src/components/grids/TwoCol.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './twocol.scss';
+import { Button } from '../buttons/Button'
 
-export const TwoCol = ({ img, reverse, primaryText, secondaryText, color, background }) => {
+export const TwoCol = ({ img, reverse, primaryText, secondaryText, color, background, cta }) => {
     return (
         <div className="grid-container">
             <div className={ `grid two-col ${reverse ? 'reverse' : ''}` }>
                 <div className="left-block" style={{ color, background }}>
                     <h1>{ primaryText }</h1>
                     <p>{ secondaryText }</p>
+                    {
+                        cta &&
+                        <div className="cta">
+                            <Button label="Learn More" size="large" backgroundColor="#fff" />
+                        </div>
+                    }
                 </div>
                 <div className="right-block">
                     <img alt="" className="responsive" src={ img } />
@@ -25,13 +32,15 @@ TwoCol.propTypes = {
     secondaryText: PropTypes.string,
     color: PropTypes.string,
     background: PropTypes.string,
+    cta: PropTypes.bool,
 };
 
 TwoCol.defaultProps = {
     reverse: false,
     img: 'https://fakeimg.pl/800x600/cccccc,128/7d7d7d,255/?text=Img&font=lobster',
     primaryText: 'Lorem ipsum dolor sit amet',
-    secondaryText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tincidunt odio sit amet nulla pellentesque lobortis. Cras porttitor tristique felis vel accumsan. Phasellus eget risus tortor. Aenean maximus cursus dolor, eget condimentum sem fringilla at. Phasellus sodales nibh ac dapibus varius. Praesent luctus congue quam at rutrum.',
+    secondaryText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tincidunt odio sit amet nulla pellentesque lobortis. Cras porttitor tristique felis vel accumsan. Phasellus eget risus tortor. Aenean maximus cursus dolor, eget condimentum sem fringilla at.',
     color: '#000',
-    background: '#fff'
+    background: '#fff',
+    cta: false
 };
